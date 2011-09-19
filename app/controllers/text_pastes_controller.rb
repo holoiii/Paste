91a155e1e72672bc7769c9930fbda6faed51962c
Albert Peng <albertpeng.ap@gmail.com>
2011-09-18 22:06:56 -0700

    Added styling and finished main functionality

--- app/controllers/text_pastes_controller.rb
+++ app/controllers/text_pastes_controller.rb
@@ -2,7 +2,7 @@ class TextPastesController < ApplicationController
   # GET /text_pastes
   # GET /text_pastes.xml
   def index
-    @recent_pastes = TextPaste.all
+    @recent_pastes = TextPaste.latest(20)
     @new_paste = TextPaste.new
   end
 
@@ -10,22 +10,20 @@ class TextPastesController < ApplicationController
   # GET /text_pastes/1.xml
   def show
     @text_paste = TextPaste.find(params[:id])
-    @recent_pastes = TextPaste.all
+    @recent_pastes = TextPaste.latest(20)
   end
 
   # POST /text_pastes
   # POST /text_pastes.xml
   def create
     @text_paste = TextPaste.new(params[:text_paste])
-    @recent_pastes = TextPaste.all
-
     if @text_paste.save
       flash[:notice] = 'Paste successfully saved!'
+      @recent_pastes = TextPaste.latest(20)
       render "show"
     else
       flash[:notice] = 'Error, paste not saved!'
       render 'index'
     end
-
   end
 end
--- app/models/text_paste.rb
+++ app/models/text_paste.rb
@@ -1,2 +1,7 @@
 class TextPaste < ActiveRecord::Base
+
+  def self.latest(limit)
+    TextPaste.find(:all, :order => "created_at DESC", :limit => limit)
+  end
+
 end
